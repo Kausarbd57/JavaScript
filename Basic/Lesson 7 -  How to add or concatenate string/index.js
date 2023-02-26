6a0c9f0b318a3b77f2c25c3dea97ecd0d9c6fabd
KAUSAR <KAUSARBD57@GMAIL.COM>
2023-02-26 14:02:14 +0600

    add more lesson

---
+++ Basic/Lesson 7 -  How to add or concatenate string/index.js
@@ -0,0 +1,26 @@
+
+/*
+var firstName="kausar";
+var lastName=" ahmed";
+
+var fullName=firstName + lastName;
+
+document.write("My Name is: "+ fullName);
+*/
+
+
+/*
+var firstName="kausar";
+var lastName=" ahmed";
+
+var fullName=firstName + lastName;
+
+document.write(fullName+" is my name.");
+*/
+
+var num1=20;
+var num2=30;
+sum=num1+num2;
+document.write(num1+num2+"<br>");
+document.write(num1+" + " +num2+" = "+sum+"<br>");
+
