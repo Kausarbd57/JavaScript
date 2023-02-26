6e012b9b6c42c70742ed058353f6f447c8326ae9
KAUSAR <KAUSARBD57@GMAIL.COM>
2023-02-26 15:02:45 +0600

    concat abd slice

---
+++ Basic/Lesson 8 - Library Function for String/index.js
@@ -0,0 +1,26 @@
+/*
+var text="bangladesh"
+var len=text.length;
+document.write("Number of charecter: "+len);
+*/
+
+/*
+var text = prompt("Enter Your Name: ");
+document.write("Number of charecter : "+text.length);
+*/
+
+/*
+var text = prompt("Enter Your Name:  ");
+document.write("Number of charecter : "+text.length+"<br>");
+document.write(text.charAt(2));
+*/
+
+/*
+var t1="Bangladesh";
+var t2=" is a beutiful country";
+document.write(t1.concat(t2));
+*/
+
+var t1="Bangladesh";
+var text=t1.slice(1,9)
+document.write(text);
