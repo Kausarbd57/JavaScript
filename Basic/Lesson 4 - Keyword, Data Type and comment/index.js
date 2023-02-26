6707b257adf8525455eb4e7684ebca6a80a7e038
KAUSAR <KAUSARBD57@GMAIL.COM>
2023-02-26 12:58:21 +0600

    add more lesson

---
+++ Basic/Lesson 4 - Keyword, Data Type and comment/index.js
@@ -0,0 +1,6 @@
+
+/*
+//document.write('kausar<br>');
+document.write("123");
+document.write("");
+*/
---
+++ Basic/Lesson 5 - How to declare variables/index.js
@@ -0,0 +1,13 @@
+var name;
+var age;
+var mail;
+
+
+name="KAUSAR AHMED";
+age= 25;
+mail="kausarbd57@gmail.com"
+
+
+document.write(name +"<br>");
+document.write(age+"<br>");
+document.write(mail);s
