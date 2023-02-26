1ea340ac4b3080e0cebd6d4e64be24df4d622b3d
KAUSAR <KAUSARBD57@GMAIL.COM>
2023-02-26 22:05:16 +0600

    tempurature calculator

---
+++ Basic/Lesson 12 - Temperature converter/index.js
@@ -0,0 +1,18 @@
+//---------Celsius------------
+
+Fahrenheit=prompt("Fahrenheit Temperature ");
+Fahrenheit=parseFloat(Fahrenheit);
+
+var Celsius=(Fahrenheit-32)*5/9;
+document.write("Tempeteture in Celsius: "+Celsius);
+
+
+
+//--------Fahrenheit----------
+
+Celsius=prompt("Celsius Temperature ");
+Celsius=parseFloat(Celsius);
+
+var Fahrenheit=(Celsius*9/5)+32;
+document.write("<br>Tempeteture in Fahrenheit: "+Fahrenheit);
+
